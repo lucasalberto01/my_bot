ee4feed50a274635ef56e71f4445c94b6e1b507f
Lucas Alberto <lucas.alberto98@gmail.com>
2024-02-07 19:59:21 -0300

    feat: disable control in robot

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -41,13 +41,13 @@ def generate_launch_description():
     #             )])
     # )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
-    twist_mux = Node(
-        package="twist_mux",
-        executable="twist_mux",
-        parameters=[twist_mux_params],
-        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
-    )
+    # twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
+    # twist_mux = Node(
+    #     package="twist_mux",
+    #     executable="twist_mux",
+    #     parameters=[twist_mux_params],
+    #     remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
+    # )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
@@ -108,7 +108,7 @@ def generate_launch_description():
         rsp,
         # lidar,
         # joystick,
-        twist_mux,
+        # twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
